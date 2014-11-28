c253d1ff4a37906b4fd0ebb74830946f3f0b440f
foospidy <foospidy@users.noreply.github.com>
2014-11-28 09:39:56 -0500

    Update top-ip.php

--- bin/top-ip.php
+++ bin/top-ip.php
@@ -1,8 +1,34 @@
 <?php
-$rs = $db->Execute("SELECT remote_host, COUNT(remote_host) AS ip_count FROM honeypy GROUP BY remote_host ORDER BY ip_count DESC LIMIT 10;");
+// perform validation
+if(isset($_GET['b'])) {
+	if(false == filter_var($_GET['b'], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[a-z-]{1,15}$/')))) {
+		echo 'b:Error. Meh!';
+		exit();
+	}
+}
+
+if(isset($_GET['days'])) {
+	if(false == filter_var($_GET['days'], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[0-9]{1,3}$/')))) {
+		echo 'days:Error. Meh!';
+		exit();
+	}
+	
+	$days = $_GET['days'];
+} else {
+	$days = 10;
+}
+
+switch($_GET['b']) {
+	case 'days':
+		$params = array($days);
+		$rs = $db->Execute("SELECT remote_host, COUNT(remote_host) AS ip_count FROM honeypy WHERE date >= (CURDATE() - INTERVAL ? DAY) GROUP BY remote_host ORDER BY ip_count DESC LIMIT 10;", $params);
+		break;
+	default:
+		$rs = $db->Execute("SELECT remote_host, COUNT(remote_host) AS ip_count FROM honeypy GROUP BY remote_host ORDER BY ip_count DESC LIMIT 10;");
+		break;
+}
 
 $ipArray = array();
-
 foreach($rs as $row) {
 	array_push($ipArray, $row);
 }
