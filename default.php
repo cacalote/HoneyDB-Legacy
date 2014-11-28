d351a09fa5531f55787f5f559ae0747c532ede7b
foospidy <foospidy@users.noreply.github.com>
2014-11-28 09:38:37 -0500

    Update default.php

--- default.php
+++ default.php
@@ -4,101 +4,209 @@
 	<div id="chartdiv" style="height:200px;width:800px;margin-bottom:30px;"></div>
 	<table align="center" width="90%">
 	<tr>
-		<td align="center">Top Offending Hosts</td><td align="center">Top Attacked Services</td>
+		<td align="center">Top Attack Hosts<br><small>(last 10 days)</small></td>
+		<td align="center">Top Attacked Services<br><small>(last 10 days)</small></td>
 	</tr>
-		<td id="top-ip" valign="top" align="center" width="50%"></td>
-		<td id="top-service" valign="top" align="center"></td>
+		<td id="top-ip-chart" valign="top" align="center" width="50%"></td>
+		<td id="top-service-chart" valign="top" align="center"></td>
+	</tr>
+	</tr>
+		<td id="top-ip-chart-mouseover" valign="top" align="center" width="50%"></td>
+		<td id="top-service-chart-mouseover" valign="top" align="center"></td>
 	</tr>
 	</table>
 </div>
+
+<div id="top-ip" style="display:none;"></div>
+<div id="top-service" style="display:none;"></div>
 
 </div><!-- /.container -->
 
 <script language="javascript">
 $(document).ready(function() {
+	// get top list of top ip and top service
+	document.getElementById('top-ip').innerHTML      = getTopIp(10);
+	document.getElementById('top-service').innerHTML = getTopService(10);
 
+	// increase font size of first row
+	$('#top-ip-chart-mouseover').css('font-size', '30px');
+	$('#top-service-chart-mouseover').css('font-size','30px');
 
-document.getElementById('top-ip').innerHTML      = getTopIp();
-document.getElementById('top-service').innerHTML = getTopService();
+	// get flag/country for each ip
+	i = 0;
+	$('#top-ip').children('div').each(function(i) {
+			ip = $(this).text().split('(');
+			$.ajax({
+					async:    false,
+					dataType: 'json',
+					url:      'geoip/' + ip[0],
+					success:  function(data) {
+							html = '<div style="font-size:small;"><img src="https://foospidy.com/opt/honeydb/img/flags/' + data['countryIsoCode'].toLowerCase() + '.png"> ' + data['countryName'] + '</div>';
+							$('#top-ip-' + i).append(html);
+					}
+			});
+			i++;
+	});
 
-$('#top-ip-1').css('font-size', '30px');
-$('#top-service-1').css('font-size','30px');
+	// charting
+	s1 = []; // series data
+	t1 = []; // tick data
+	$.ajax({
+			async:    false,
+			dataType: 'json',
+			url:      'chart-data/bar',
+			success:  function(data) {
+					$.each(data, function() {
+							s1.push(this['day_count']);
+							t1.push(this['day']);
+					});
+			}
+	});
 
-/*
-i = 0;
-$('#top-ip').children('div').each(function(i) {
-        i++;
-        ip = $(this).text().split('(');
-        $.ajax({
-                async:    false,
-                dataType: 'json',
-                url:      'geoip/' + ip[0],
-                success:  function(data) {
-                        html = '<div style="font-size:small;"><img src="http://foospidy.com/opt/honeydb/img_flags/' + data['countryIsoCode'].toLowerCase() + '.png"> ' + data['countryName'] + '</div>';
-                        $('#top-ip-' + i).append(html);
-                }
-        });
-});
-*/
-
-// charting
-s1 = []; // series data
-t1 = []; // tick data
-$.ajax({
-        async:    false,
-        dataType: 'json',
-        url:      'chart-data/bar',
-        success:  function(data) {
-                $.each(data, function() {
-                        s1.push(this['day_count']);
-                        t1.push(this['day']);
-                });
-        }
-});
-
-console.log(s1);
-var plot1 = $.jqplot('chartdiv', [s1], {
-        seriesDefaults:{
-                renderer:$.jqplot.BarRenderer,
-                rendererOptions: {fillToZero: true}
-        },
-        axes: {
-            // Use a category axis on the x axis and use our custom ticks.
-            xaxis: {
-                renderer: $.jqplot.CategoryAxisRenderer,
-                ticks: t1
-/*
-		tickOptions: {
-			show:false
+	// main chart
+	var plot1 = $.jqplot('chartdiv', [s1], {
+		seriesDefaults:{
+				renderer:$.jqplot.BarRenderer,
+				rendererOptions: {fillToZero: true}
 		},
-		rendererOptions: {
-			drawBaseline:false
+        axes: {
+			// Use a category axis on the x axis and use our custom ticks.
+			xaxis: {
+				renderer: $.jqplot.CategoryAxisRenderer,
+				ticks: t1
+			/*
+			tickOptions: {
+				show:false
+			},
+			rendererOptions: {
+				drawBaseline:false
+			}
+			*/
+			},
+			// Pad the y axis just a little so bars can get close to, but
+			// not touch, the grid boundaries.  1.2 is the default padding.
+			yaxis: {
+				pad: 1.05,
+				tickOptions: {formatString: '%d'},
+				min:0,
+				max:1000
+			}
+		},
+		grid: {borderColor: 'white', shadow: false, drawBorder: true}
+	});
+	
+	// top ip chart
+	var pie_data = [];
+	
+	$.ajax({
+			async:    false,
+			dataType: 'json',
+			url:      'top-ip/days/10',
+			success:  function(data) {
+					$.each(data, function() {
+							ip = [];
+							ip.push(this['remote_host'].toString());
+							ip.push(parseInt(this['ip_count']));
+							pie_data.push(ip);
+					});
+			}
+	});
+
+	var plot2 = jQuery.jqplot ('top-ip-chart', [pie_data], { 
+		seriesDefaults: {
+			// Make this a pie chart.
+			renderer: jQuery.jqplot.PieRenderer,
+			shadow: true,
+			rendererOptions: {
+			  // Put data labels on the pie slices.
+			  // By default, labels show the percentage of the slice.
+			  showDataLabels: true
+			}
+		}, 
+		legend: { show:true, location: 'w' },
+		grid: {borderColor: 'white', shadow: false, drawBorder: true}
+	});
+	
+	// top service chart
+	var pie_data = [];
+
+	$.ajax({
+			async:    false,
+			dataType: 'json',
+			url:      'top-service/days/10',
+			success:  function(data) {
+					$.each(data, function() {
+							service = [];
+							service.push(this['service'].toString());
+							service.push(parseInt(this['service_count']));
+							pie_data.push(service);
+					});
+			}
+	});
+
+	var plot3 = jQuery.jqplot ('top-service-chart', [pie_data], { 
+		seriesDefaults: {
+			// Make this a pie chart.
+			renderer: jQuery.jqplot.PieRenderer,
+			shadow: true,
+			rendererOptions: {
+			  // Put data labels on the pie slices.
+			  // By default, labels show the percentage of the slice.
+			  showDataLabels: true,
+			  highlightMouseOver: true
+			},
+			highlighter: {
+				show: true,
+				showTooltip: true,
+				tooltipFade: true,
+				formatString: '%s', 
+				tooltipLocation: 'sw', 
+				useAxesFormatters: false
+			}
+		}, 
+		legend: { show:true, location: 'w' },
+		grid: {borderColor: 'white', shadow: false, drawBorder: true}
+	});
+
+	// mouseover on charts
+	$('#top-ip-chart').bind('jqplotDataHighlight', 
+		function (ev, seriesIndex, pointIndex, data) {
+			$('#top-ip-chart-mouseover').html($('#top-ip-' + pointIndex).html());
 		}
-*/
-            },
-            // Pad the y axis just a little so bars can get close to, but
-            // not touch, the grid boundaries.  1.2 is the default padding.
-            yaxis: {
-                pad: 1.05,
-                tickOptions: {formatString: '%d'},
-		min:0,
-		max:1000
-            }
-        }
-});
+	);
 
-$('#top-ip').children('div').click(function(event) {
-        ip = $(event.target).text().split(' (');
-        url = 'view-ip/' + ip[0];
-        location.href = url;
-});
+	$('#top-service-chart').bind('jqplotDataHighlight', 
+		function (ev, seriesIndex, pointIndex, data) {
+			info = data.toString().split(',');
+			$('#top-service-chart-mouseover').html(info[0] + '<br><small>(' + info[1] + ')</small>');
+		}
+	);
 
-$('#top-service').children('div').click(function(event) {
-        service = $(event.target).text().split(' (');
-        url = 'view-service/' + service[0].replace(/\[/g, '').replace(/\]/g, '');
-        location.href = url;
-});
+	// click on charts
+	$('#top-ip-chart').bind('jqplotDataClick', 
+		function (ev, seriesIndex, pointIndex, data) {
+			url = 'view-ip/' + data[0].replace(/\[/g, '').replace(/\]/g, '');
+			location.href = url;
+		}
+	);
 
+	$('#top-service-chart').bind('jqplotDataClick', 
+		function (ev, seriesIndex, pointIndex, data) {
+			url = 'view-service/' + data[0].replace(/\[/g, '').replace(/\]/g, '');
+			location.href = url;
+		}
+	);
+
+	$('#top-ip').children('div').click(function(event) {
+			ip = $(event.target).text().split(' (');
+			url = 'view-ip/' + ip[0];
+			location.href = url;
+	});
+
+	$('#top-service').children('div').click(function(event) {
+			service = $(event.target).text().split(' (');
+			url = 'view-service/' + service[0].replace(/\[/g, '').replace(/\]/g, '');
+			location.href = url;
+	});
 });
 </script>
-
