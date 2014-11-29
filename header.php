a6b9dabb053b35695143723f28367e54f8cbf5b6
foospidy <foospidy@users.noreply.github.com>
2014-11-29 09:48:47 -0500

    Update header.php

--- header.php
+++ header.php
@@ -65,3 +65,5 @@ $(document).ready(function() {
         </div><!--/.nav-collapse -->
       </div>
 </div>
+
+<div class="container" align="right" style="margin-top:10px;"><?php echo get_days_dropdown(); ?> </div>
