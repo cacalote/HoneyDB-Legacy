f4ab3ce6fcf5844b5564ae47fff3bf41e8f5cac6
foospidy <foospidy@www.foospidy.com>
2014-11-30 20:53:06 -0500

    seperate config from index.php

---
+++ etc/configuration.php
@@ -0,0 +1,15 @@
+<?php
+// constants
+$WEBROOT        = '/honeydb/';
+$SHODAN_API_KEY = '';
+$PH_API_KEY     = '';
+$LOGGER_ENABLE  = 'No';
+$LOGGER_SECRET  = ''; // suggestion, generate secret from https://www.grc.com/passwords.htm
+$LOGGER_FILTER  = ''; // optional, comma delimated ip address list
+$DB_HOST        = 'localhost';
+$DB_NAME        = '';
+$DB_USER        = '';
+$DB_PASS        = '';
+$DEFAULT_DAYS   = 10;
+?>
+
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 include 'common-functions.php';
-
-// constants
-$WEBROOT        = '/honeydb/';
-$SHODAN_API_KEY = '';
-$PH_API_KEY     = '';
-$LOGGER_ENABLE  = 'No';
-$LOGGER_SECRET  = ''; // suggestion, generate secret from https://www.grc.com/passwords.htm
-$LOGGER_FILTER  = ''; // optional, comma delimated ip address list
-$DB_HOST        = 'localhost';
-$DB_NAME        = '';
-$DB_USER        = '';
-$DB_PASS        = '';
-$DEFAULT_DAYS   = 10;
+include 'etc/configuration.php';
 
 if(!isset($_GET['c'])) { $_GET['c'] = 'default'; }
 
