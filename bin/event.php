12cdc5b235369e4d986cf07d3be1d4f41f372016
foospidy <foospidy@users.noreply.github.com>
2014-12-12 05:38:50 -0500

    Update event.php

--- bin/event.php
+++ bin/event.php
@@ -1,9 +1,11 @@
 <?php
 include 'validate.services.php';
 include 'validate.ip.php';
+include 'validate.days.php';
 
-$where = '';
-$paramArray = array();
+$where       = '';
+$where_count = 0;
+$paramArray  = array();
 
 if($s != '' || $i != '') {
 	$where = 'WHERE ';
@@ -11,14 +13,29 @@ if($s != '' || $i != '') {
 	if(strlen(trim($s)) && strlen(trim($i))) {
 		$where .= "service=? AND remote_host=?";
 		array_push($paramArray, '[' . $s . ']', $i);
+		$where_count++;
 	} elseif(strlen(trim($s))) {
 		$where .= "service=?";
 		array_push($paramArray, '[' . $s . ']');
+		$where_count++;
 	} elseif(strlen(trim($i))) {
 		$where .= "remote_host=?";
 		array_push($paramArray, $i);
+		$where_count++;
 	}
 	// else don't do nada
+}
+
+if($days > 0) {
+	if(0 == $where_count) {
+		$where .= " WHERE";
+		$where_count++;
+	} else {
+		$where .= " AND";
+	}
+
+	$where .= " date >= (CURDATE() - INTERVAL ? DAY) ";
+	array_push($paramArray, $days);
 }
 
 $rs = $db->Execute("SELECT date_time, event, data, bytes FROM honeypy $where ORDER BY date_time ASC;", $paramArray);
