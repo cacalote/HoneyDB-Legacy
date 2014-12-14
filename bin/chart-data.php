d9b40d535db35ace03d82069e7513464c0717ee5
foospidy <foospidy@users.noreply.github.com>
2014-12-14 11:28:08 -0500

    Update chart-data.php, changing metric to CONNECTs

--- bin/chart-data.php
+++ bin/chart-data.php
@@ -9,7 +9,7 @@ if(isset($_REQUEST['days'])) {
 switch($_GET['a']) {
 	case 'bar':
 		$params = array($days);
-		$sql    = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d') AS day, COUNT(*) AS day_count FROM honeypy l ";
+		$sql    = "SELECT DATE_FORMAT(date_time, '%Y-%m-%d') AS day, COUNT(*) AS day_count FROM honeypy WHERE event='CONNECT' ";
 		$sql   .= "GROUP BY DATE_FORMAT(date_time, '%Y-%m-%d') ORDER BY date_time DESC LIMIT ?;";
 		$rs     = $db->Execute($sql, $params);
 
