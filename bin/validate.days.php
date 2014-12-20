2899315c75539e4898e6abd3cbaf3406ea5a7a52
foospidy <foospidy@users.noreply.github.com>
2014-12-20 09:27:08 -0500

    Update validate.days.php

--- bin/validate.days.php
+++ bin/validate.days.php
@@ -1,8 +1,10 @@
 <?php
-$days = !isset($_REQUEST['days']) ? $DEFAULT_DAYS : trim($_REQUEST['days']);
+$days = !isset($_REQUEST['days']) ? $session->get_sessionvar('days') : trim($_REQUEST['days']);
 
 if(false == filter_var($days, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[0-9]{1,3}$/')))) {
 	echo 'days:Error. Meh!';
 	exit();
+} else {
+	$session->set_sessionvar('days', $days);
 }
 ?>
