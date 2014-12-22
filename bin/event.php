b161036d45eca5a381cc0b9b8ba2bb0575739529
foospidy <foospidy@users.noreply.github.com>
2014-12-22 15:54:14 -0500

    Update event.php

--- bin/event.php
+++ bin/event.php
@@ -1,6 +1,6 @@
 <?php
 include 'validate.date.php';
-include 'validate.services.php';
+include 'validate.service.php';
 include 'validate.ip.php';
 include 'validate.days.php';
 
@@ -8,35 +8,23 @@ $where       = '';
 $where_count = 0;
 $paramArray  = array();
 
-if($s != '' || $i != '') {
+if('all' != $s || 'all' != $i) {
 	$where = 'WHERE ';
 
-	if(strlen(trim($s)) && strlen(trim($i))) {
+	if('all' != $s && 'all' != $i) {
 		$where .= "service=? AND remote_host=?";
 		array_push($paramArray, '[' . $s . ']', $i);
 		$where_count++;
-	} elseif(strlen(trim($s))) {
+	} elseif('all' != strlen(trim($s))) {
 		$where .= "service=?";
 		array_push($paramArray, '[' . $s . ']');
 		$where_count++;
-	} elseif(strlen(trim($i))) {
+	} elseif('all' != strlen(trim($i))) {
 		$where .= "remote_host=?";
 		array_push($paramArray, $i);
 		$where_count++;
 	}
 	// else don't do nada
-}
-
-if($days > 0) {
-	if(0 == $where_count) {
-		$where .= " WHERE";
-		$where_count++;
-	} else {
-		$where .= " AND";
-	}
-
-	$where .= " date >= (CURDATE() - INTERVAL ? DAY) ";
-	array_push($paramArray, $days);
 }
 
 if('all' != $date) {
@@ -49,6 +37,18 @@ if('all' != $date) {
 
 	$where .= " date = ? ";
 	array_push($paramArray, $date);
+} else {
+	if($days > 0) {
+		if(0 == $where_count) {
+			$where .= " WHERE";
+			$where_count++;
+		} else {
+			$where .= " AND";
+		}
+
+		$where .= " date >= (CURDATE() - INTERVAL ? DAY) ";
+		array_push($paramArray, $days);
+	}
 }
 
 $rs = $db->Execute("SELECT date_time, event, data, bytes FROM honeypy $where ORDER BY date_time ASC;", $paramArray);
