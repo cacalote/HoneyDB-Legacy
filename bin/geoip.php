94b5e1d7cbc1544ba0cb5c39eb5c3cca1a74a76b
foospidy <foospidy@users.noreply.github.com>
2014-12-22 21:55:35 -0500

    Update geoip.php

--- bin/geoip.php
+++ bin/geoip.php
@@ -1,47 +1,36 @@
 <?php
 use GeoIp2\Database\Reader;
-
-$a = !isset($_GET['a']) ? $_GET['a'] = 'all' : trim($_GET['a']);
+include 'validate.ip.php';
 
 $reader = new Reader('usr/data/GeoLite2-City.mmdb');
 
-// perform validation
-if(false == filter_var($_GET['i'], FILTER_VALIDATE_IP)) {
-        echo 'i:Error, meh!';
-        exit();
+try {
+	$record     = $reader->city($_GET['i']);
+
+	$geoIpArray = array(
+		'countryIsoCode'     => $record->country->isoCode,
+		'countryName'        => $record->country->name,
+		'subdivisionIsoCode' => $record->mostSpecificSubdivision->isoCode,
+		'subdivisionName'    => $record->mostSpecificSubdivision->name,
+		'cityName'           => $record->city->name,
+		'postalCode'         => $record->postal->code,
+		'latitude'           => $record->location->latitude,
+		'longitude'          => $record->location->longitude
+		);
+} catch (Exception $e) {
+	// error reading data or ip not found in db
+	$geoIpArray = array(
+		'countryIsoCode'     => 'unknown',
+		'countryName'        => 'unknown',
+		'subdivisionIsoCode' => 'unknown',
+		'subdivisionName'    => 'unknown',
+		'cityName'           => 'unknown',
+		'postalCode'         => 'unknown',
+		'latitude'           => 'unknown',
+		'longitude'          => 'unknown'
+		);
 }
 
-switch($a) {
-	default:
-		try {
-			$record     = $reader->city($_GET['i']);
-
-			$geoIpArray = array(
-				'countryIsoCode'     => $record->country->isoCode,
-				'countryName'        => $record->country->name,
-				'subdivisionIsoCode' => $record->mostSpecificSubdivision->isoCode,
-				'subdivisionName'    => $record->mostSpecificSubdivision->name,
-				'cityName'           => $record->city->name,
-				'postalCode'         => $record->postal->code,
-				'latitude'           => $record->location->latitude,
-				'longitude'          => $record->location->longitude
-				);
-		} catch (Exception $e) {
-			// error reading data or ip not found in db
-			$geoIpArray = array(
-				'countryIsoCode'     => 'unknown',
-				'countryName'        => 'unknown',
-				'subdivisionIsoCode' => 'unknown',
-				'subdivisionName'    => 'unknown',
-				'cityName'           => 'unknown',
-				'postalCode'         => 'unknown',
-				'latitude'           => 'unknown',
-				'longitude'          => 'unknown'
-				);
-		}
-
-		header("Content-type: text/plain");
-		echo json_encode($geoIpArray);
-		break;
-}
+header("Content-type: text/plain");
+echo json_encode($geoIpArray);
 ?>
