1f3fa3000b04ad102ef2adad2883a6bdd0853044
foospidy <foospidy@users.noreply.github.com>
2015-01-11 14:00:32 -0500

    Update top-ip.php

--- bin/top-ip.php
+++ bin/top-ip.php
@@ -2,7 +2,7 @@
 include 'validate.days.php';
 
 $params  = array($days);
-$rs      = $db->Execute("SELECT remote_host, COUNT(remote_host) AS ip_count FROM honeypy WHERE date >= (CURDATE() - INTERVAL ? DAY) AND event='CONNECT' GROUP BY remote_host ORDER BY ip_count DESC LIMIT 10;", $params);
+$rs      = $db->CacheExecute(300, "SELECT remote_host, COUNT(remote_host) AS ip_count FROM honeypy WHERE date >= (CURDATE() - INTERVAL ? DAY) AND event='CONNECT' GROUP BY remote_host ORDER BY ip_count DESC LIMIT 10;", $params);
 $ipArray = array();
 
 foreach($rs as $row) {
