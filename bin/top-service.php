1485195d1544e5f39c33b2e974df17714a00b40c
foospidy <foospidy@users.noreply.github.com>
2015-01-11 14:00:52 -0500

    Update top-service.php

--- bin/top-service.php
+++ bin/top-service.php
@@ -3,7 +3,7 @@ include 'validate.service.php';
 include 'validate.days.php';
 
 $params  = array($days);
-$rs      = $db->Execute("SELECT service, COUNT(service) AS service_count FROM honeypy WHERE date >= (CURDATE() - INTERVAL ? DAY) AND event='CONNECT' GROUP BY service ORDER BY service_count DESC LIMIT 10;", $params);
+$rs      = $db->CacheExecute(300, "SELECT service, COUNT(service) AS service_count FROM honeypy WHERE date >= (CURDATE() - INTERVAL ? DAY) AND event='CONNECT' GROUP BY service ORDER BY service_count DESC LIMIT 10;", $params);
 $ipArray = array();
 
 foreach($rs as $row) {
