3b54f1426dfd36717b70bff61fc2663c3eb0feb6
foospidy <foospidy@users.noreply.github.com>
2015-01-11 13:54:15 -0500

    Update configuration.php

--- etc/configuration.php
+++ etc/configuration.php
@@ -1,6 +1,7 @@
 <?php
 // constants
 $VERSION        = '0.0.1';
+$ROOTDIR        = '/var/www/honeydb/';
 $WEBROOT        = '/honeydb/';
 $SHODAN_API_KEY = '';
 $PH_API_KEY     = '';
@@ -12,6 +13,7 @@ $DB_NAME        = '';
 $DB_USER        = '';
 $DB_PASS        = '';
 
+$ADODB_CACHE_DIR    = $ROOTDIR . 'cache';
 $DEFAULT_DAYS       = 10;
 $CHART_DAILY_Y_MAX  = 5000;
 ?>
