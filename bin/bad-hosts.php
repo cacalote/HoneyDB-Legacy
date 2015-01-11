9bf39d88caece9ac42d9399cf4d0a9a5b64b4cc6
foospidy <foospidy@users.noreply.github.com>
2015-01-11 13:59:11 -0500

    Update bad-hosts.php

--- bin/bad-hosts.php
+++ bin/bad-hosts.php
@@ -1,12 +1,11 @@
 <?php
-$rs = $db->Execute("SELECT remote_host, COUNT(remote_host) AS ip_count, MAX(date) AS last_seen FROM honeypy GROUP BY remote_host ORDER BY ip_count DESC;");
-
-$ipArray = array();
+$rs        = $db->CacheExecute(300, "SELECT remote_host, COUNT(remote_host) AS ip_count, MAX(date) AS last_seen FROM honeypy GROUP BY remote_host ORDER BY ip_count DESC;");
+$bad_hosts = array();
 
 foreach($rs as $row) {
-	array_push($ipArray, array('remote_host'=>$row['remote_host'], 'count'=>$row['ip_count'], 'last_seen'=>$row['last_seen']));
+	array_push($bad_hosts, array('remote_host'=>$row['remote_host'], 'count'=>$row['ip_count'], 'last_seen'=>$row['last_seen']));
 }
 
-header("Content-type: text/plain");
-echo json_encode($ipArray);
+header("Content-type: application/json");
+echo json_encode($bad_hosts);
 ?>
